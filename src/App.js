6007874b589a4daff913d6f4a441161fd7114de4
Ephraim Park <ephiepark@gmail.com>
2022-07-31 00:28:35 -0700

    Delete Firestore data when a todo item is removed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Button from '@mui/material/Button';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, addDoc, setDoc, doc } from "firebase/firestore";
+import { getFirestore, collection, addDoc, setDoc, doc, deleteDoc } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -101,7 +101,9 @@ function App() {
     }));
   };
 
-  const onRemoveClick = (removedTodoItem) => {
+  const onRemoveClick = async (removedTodoItem) => {
+    const todoItemRef = doc(db, "todoItem", removedTodoItem.id);
+    await deleteDoc(todoItemRef);
     setTodoItemList(todoItemList.filter((todoItem) => {
       return todoItem.id !== removedTodoItem.id;
     }));
