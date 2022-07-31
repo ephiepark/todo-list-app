87e84a7d05354228dae80f1404ba2b66afbcbb38
Ephraim Park <ephiepark@gmail.com>
2022-07-30 22:01:02 -0700

    [4/n] Create TodoItemInputField: Add onSubmit callback as props to TodoItemInputField

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Button from '@mui/material/Button';
 const TodoItemInputField = (props) => {
   const [input, setInput] = useState("");
 
+  const onSubmit = () => {
+    props.onSubmit(input);
+    setInput("");
+  };
+
   return (<div>
     <TextField
       id="todo-item-input"
@@ -13,14 +18,14 @@ const TodoItemInputField = (props) => {
       variant="outlined"
       onChange={(e) => setInput(e.target.value)} value={input}
     />
-    <Button variant="outlined">Submit</Button>
+    <Button variant="outlined" onClick={onSubmit}>Submit</Button>
   </div>);
 };
 
 function App() {
   return (
     <div className="App">
-      <TodoItemInputField />
+      <TodoItemInputField onSubmit={() => {}} />
     </div>
   );
 }
