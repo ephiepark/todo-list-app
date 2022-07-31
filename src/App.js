b3b2c9737deb0ffd8f25280a365f503b472dea45
Ephraim Park <ephiepark@gmail.com>
2022-07-31 11:48:09 -0700

    Add timestamp to a todo item

--- src/App.js
+++ src/App.js
@@ -81,6 +81,7 @@ function App() {
           id: doc.id,
           todoItemContent: doc.data().todoItemContent,
           isFinished: doc.data().isFinished,
+          createdTime: doc.data().createdTime ?? 0,
         });
       });
       setTodoItemList(firestoreTodoItemList);
@@ -94,6 +95,7 @@ function App() {
     await addDoc(collection(db, "todoItem"), {
       todoItemContent: newTodoItem,
       isFinished: false,
+      createdTime: Math.floor(Date.now() / 1000),
     });
     syncTodoItemListStateWithFirestore();
   };
