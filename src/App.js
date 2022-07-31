6ce4e0a93a0eba60ef3acbbf67b88b2c34491608
Ephraim Park <ephiepark@gmail.com>
2022-07-30 21:49:12 -0700

    [1/n] Create TodoItemInputField: Create a placeholder react component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 
+const TodoItemInputField = (props) => {
+  return (<div>
+  </div>);
+};
+
 function App() {
   return (
     <div className="App">
+      <TodoItemInputField />
     </div>
   );
 }
